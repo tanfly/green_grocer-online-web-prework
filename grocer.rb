f4b84af8e36c1b916405a17c6b4a50a25c0965b3
Stella McEwen <mcewen.stella@gmail.com>
2019-04-24 19:52:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def checkout(cart, coupon)
    cart = apply_coupons(cart, coupon)
    cart_discount = apply_clearance(cart)
    if cart.length > 1 
-     cart_clearance.map do |item, price_hash|
+     cart_discount.map do |item, price_hash|
        if price_hash[:count] >= 1 
          total += (price_hash[:price] * price_hash[:count])
        end
